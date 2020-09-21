1770c1d00ba1c352a4a081d747dfadbebeeeba04
chen-dudu <liguoc@student.unimelb.edu.au>
2020-09-21 19:23:43 +0800

    changed to ant design components

--- src/Login/Body.js
+++ src/Login/Body.js
@@ -1,9 +1,11 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { BodyWrapper, SubmitButton, Input, GoSignup} from './Login.style';
+import { BodyWrapper, SubmitButton, GoSignup} from './Login.style';
 import noteImg from "../img/note.png"
 
 import UserManager from "../DataModel/UserModel/UserManager";
+import { Input, Button } from 'antd';
+import 'antd/dist/antd.css';
 
 const CLASS_NAME = "Login/Body";
 
@@ -58,8 +60,9 @@ export class Body extends React.Component {
                         <label htmlFor="email" style={{ fontSize: "25px" }}>Email</label>
                     </div>
 
+                    <Input size={"large"} placeholder={'Email'} allowClear value={this.state.email} onChange={this.onEmailEnter} style={{width: '50%'}}/>
                     {/*<Input type="text" id="email" name="email" style={{ width: "50%" }} />*/}
-                    <input type={'text'} id={'email'} name={'email'} value={this.state.email} style={{width: '50%', height: 30}} onChange={this.onEmailEnter} />
+                    {/*<input type={'text'} id={'email'} name={'email'} value={this.state.email} style={{width: '50%', height: 30}} onChange={this.onEmailEnter} />*/}
                     <br />
                     <br />
                     <br />
@@ -67,21 +70,26 @@ export class Body extends React.Component {
                         <label htmlFor="password" style={{ fontSize: "25px" }}>Password</label>
                     </div>
 
+                    <Input.Password size={"large"} placeholder={'Password'} value={this.state.password} onChange={this.onPasswordEnter} style={{width: '50%'}}/>
                     {/*<Input type="password" id="password" name="password" style={{ width: "50%" }} />*/}
-                    <input type={'password'} id={'password'} name={'password'} value={this.state.password} style={{width: '50%', height: 30}} onChange={this.onPasswordEnter}/>
+                    {/*<input type={'password'} id={'password'} name={'password'} value={this.state.password} style={{width: '50%', height: 30}} onChange={this.onPasswordEnter}/>*/}
                     <br />
                     <br />
                     <br />
                     <br />
-                    <div align={'center'} style={{ width: '90%'}}>
-                        <input align={'center'} id={'login-button'} style={{background: "#3399ff", borderRadius: 5,
-                            width: "80%", height: 40, fontWeight: "bold",
-                            fontSize: 20, color: "white"}}
-                               type={'submit'} value={'Log in'} />
+                    <div align={'center'} style={{width: "90%"}} >
+                        <Button htmlType={"submit"} id={'login-button'} type={"primary"} block size={"large"} style={{fontSize: 18, fontWeight: "bold", background: "#3399ff", borderRadius: 5}}>
+                            Log in
+                        </Button>
+
+                        {/*<input align={'center'} id={'login-button'} style={{background: "#3399ff", borderRadius: 5,*/}
+                        {/*    width: "80%", height: 40, fontWeight: "bold",*/}
+                        {/*    fontSize: 20, color: "white"}}*/}
+                        {/*       type={'submit'} value={'Log in'} />*/}
                     </div>
                 </form>
                 {/*<SubmitButton>Log In</SubmitButton>*/}
-                {/* todolist: domain name need to be updated after deploying*/}
+                 {/*todolist: domain name need to be updated after deploying*/}
                 <GoSignup><a href = 'http://localhost:3000/signup'>Don't have an account? Sign up</a></GoSignup>
                 <br />
                 <br />
