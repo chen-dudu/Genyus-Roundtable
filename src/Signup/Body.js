c23e0e58ef7e1179bb72ae494b56e8fe2f397f57
chen-dudu <liguoc@student.unimelb.edu.au>
2020-09-21 19:58:52 +0800

    changed to ant design component

--- src/Signup/Body.js
+++ src/Signup/Body.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import {BodyWrapper,SubmitButton,Input} from './Signup.style';
+import {BodyWrapper,SubmitButton} from './Signup.style';
 import noteImg from "../img/note.png"
 import { Link } from "react-router-dom";
 
 import UserManager from "../DataModel/UserModel/UserManager";
+import { Input, Button} from 'antd';
+import 'antd/dist/antd.css';
 
 const CLASS_NAME = "Signup/Body";
 
@@ -22,7 +24,7 @@ class Body extends React.Component{
 
     signup(e) {
         e.preventDefault();
-        if (document.getElementById("password").value !== document.getElementById("re_password").value) {
+        if (document.getElementById("password").value !== document.getElementById("confirmPassword").value) {
             alert("The passwords entered does not match!");
             return;
         }
@@ -72,48 +74,57 @@ class Body extends React.Component{
                         <label htmlFor="email" style={{fontSize:"25px"}}>Email</label>
                     </div>
 
+                    <Input id={'email'} size={"large"} placeholder={'Email'} allowClear value={this.state.email} onChange={this.onEmailEnter} style={{width: '55%'}}/>
                     {/*<Input type="text" id="email" name="email" style={{width:"50%"}}></Input>*/}
-                    <input type={'text'} id={'email'} name={'email'} value={this.state.email} style={{width: '50%', height: 30}} onChange={this.onEmailEnter}/>
+                    {/*<input type={'text'} id={'email'} name={'email'} value={this.state.email} style={{width: '50%', height: 30}} onChange={this.onEmailEnter}/>*/}
                     <br />
                     <br />
                     <div align={'center'}>
                         <label htmlFor="password" style={{fontSize:"25px"}}>Password</label>
                     </div>
 
+                    <Input.Password id={'password'} size={"large"} placeholder={'Password'} value={this.state.password} onChange={this.onPasswordEnter} style={{width: '55%'}} />
                     {/*<Input type="text" id="password" name="password" style={{width:"50%"}}></Input>*/}
-                    <input type={'password'} id={'password'} name={'password'} value={this.state.password} style={{width: '50%', height: 30}} onChange={this.onPasswordEnter}/>
+                    {/*<input type={'password'} id={'password'} name={'password'} value={this.state.password} style={{width: '50%', height: 30}} onChange={this.onPasswordEnter}/>*/}
                     <br />
                     <br />
                     <div align={'center'}>
                         <label htmlFor="confirmPassword" style={{fontSize:"25px"}}>Re-enter</label>
                     </div>
 
+                    <Input.Password id={'confirmPassword'} size={"large"} placeholder={'Re-enter your password'} value={this.state.re_password} onChange={this.onRePasswordEnter} style={{width: '55%'}} />
                     {/*<Input type="text" id="confirmPassword" name="confirmPassword" style={{width:"50%"}}></Input>*/}
-                    <input type={'password'} id={'re_password'} name={'re_password'} value={this.state.re_password} style={{width: '50%', height: 30}} onChange={this.onRePasswordEnter}/>
+                    {/*<input type={'password'} id={'re_password'} name={'re_password'} value={this.state.re_password} style={{width: '50%', height: 30}} onChange={this.onRePasswordEnter}/>*/}
                     <br />
                     <br />
                     <div align={'center'}>
                         <label htmlFor="full_name" style={{fontSize:"25px"}}>Full Name</label>
                     </div>
 
+                    <Input id={'full_name'} size={"large"} allowClear placeholder={'Full Name'} value={this.state.full_name} onChange={this.onFullnameEnter} style={{width: '55%'}} />
                     {/*<Input type="text" id="full_name" name="fname" style={{width:"50%"}}></Input>*/}
-                    <input type={'text'} id={'full_name'} name={'full_name'} value={this.state.full_name} style={{width: '50%', height: 30}} onChange={this.onFullnameEnter}/>
+                    {/*<input type={'text'} id={'full_name'} name={'full_name'} value={this.state.full_name} style={{width: '50%', height: 30}} onChange={this.onFullnameEnter}/>*/}
                     <br />
                     <br />
                     <div align={'center'}>
                         <label htmlFor="nick_name" style={{fontSize:"25px"}}>Nickname</label>
                     </div>
 
+                    <Input id={'nick_name'} size={"large"} allowClear placeholder={'Nick Name'} value={this.state.nick_name} onChange={this.onNicknameEnter} style={{width: '55%'}} />
                     {/*<Input type="text" id="nname" name="nname" style={{width:"50%" }} value={nname}*/}
                     {/*  onChange={this.handleChange} ></Input>*/}
-                    <input type={'text'} id={'nick_name'} name={'nick_name'} value={this.state.nick_name} style={{width: '50%', height: 30}} onChange={this.onNicknameEnter}/>
+                    {/*<input type={'text'} id={'nick_name'} name={'nick_name'} value={this.state.nick_name} style={{width: '50%', height: 30}} onChange={this.onNicknameEnter}/>*/}
                     <br />
                     <br />
 
                     <div align={'center'} style={{width: '90%'}}>
-                        <input align={'center'} id={'signup-button'} style={{background: "#3399ff", borderRadius: 5,
-                            width: "80%", height: 40, fontWeight: "bold",
-                            fontSize: 20, color: "white"}} type={'submit'} value={'Sign up'}/>
+
+                        <Button htmlType={"submit"} id={'signup-button'} type={"primary"} block size={"large"} style={{fontSize: 18, fontWeight: "bold", background: '#3399ff', borderRadius: 5}} >
+                            Sign up
+                        </Button>
+                        {/*<input align={'center'} id={'signup-button'} style={{background: "#3399ff", borderRadius: 5,*/}
+                        {/*    width: "80%", height: 40, fontWeight: "bold",*/}
+                        {/*    fontSize: 20, color: "white"}} type={'submit'} value={'Sign up'}/>*/}
                     </div>
                 </form>
                 <br/><br/>
