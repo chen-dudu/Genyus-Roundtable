1e7b58a4dbdd987f48b75b91e10d9f478ba48463
Haichao Song <haichaos@student.unimelb.edu.au>
2020-09-23 01:17:02 +1000

    add step bar and body2

--- src/ViewAcceptedSession/Body.js
+++ src/ViewAcceptedSession/Body.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {BodyWrapper} from './ViewAcceptedSession.style';
 import {Button} from 'antd';
 import 'antd/dist/antd.css';
-import {Card} from 'antd';
+import {Card, Steps} from 'antd';
+
+const { Step } = Steps;
 
 class Body extends React.Component {
 	state = {
@@ -18,6 +20,21 @@ class Body extends React.Component {
 			<BodyWrapper>
 				<h1>Roundtable Confirmation</h1>
 				<h2 style={{fontStyle:"italic"}}>Meaningful Vocation: Stroke Recovery Stories</h2>
+
+				<Steps
+					type="navigation"
+					current={3}
+					onChange={this.onChange}
+					className="site-navigation-steps"
+				>
+					<Step status="finish" title="Learn About Roundtable" />
+					<Step status="finish" title="Login or Sign Up" />
+					<Step status="finish" title="Complete Questionnaire" />
+					<Step status="process" title="Registration Complete" />
+				</Steps>
+
+				<h1 style={{fontSize:"30px", marginLeft:"30px", fontWeight:"normal"}}>Your Confirmation</h1>
+
 			</BodyWrapper>
 
 
