90e97901ae3d0028e0a84820854fd510409e11df
Yujun Yan <yujunyan@Yujuns-MacBook-Pro.local>
2020-09-26 02:29:02 +1000

    added admin&researcher's Homepage

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import Signup from './Signup/Signup'
 import Login from './Login/Login'
 import Users from './Users/Users'
 import ProfileSetting from './ProfileSetting/ProfileSetting'
-import StatusAndRewards from './StatusAndRewards/StatusAndRewards'
+// import StatusAndRewards from './StatusAndRewards/StatusAndRewards'
+import Admins from './Admins/Admins'
+import Researchers from './Researchers/Researchers'
 
 
 function App(){
-  const [nickName, setNickName] = useState("");
+  // const [nickName, setNickName] = useState("");
   return (
     <Router>
         <Route path="/HomePage" component={HomePage}></Route>
@@ -24,7 +26,9 @@ function App(){
         <Route path="/Login" component={Login}></Route>
         <Route path="/ProfileSetting" component={ProfileSetting}></Route>
         <Route path="/users" component={Users}></Route>
-        <Route path="/StatusAndRewards" component={StatusAndRewards}></Route>
+        {/* <Route path="/StatusAndRewards" component={StatusAndRewards}></Route> */}
+        <Route path="/Admins" component={Admins}></Route>
+        <Route path="/Researchers" component={Researchers}></Route>
     </Router>
   );
 }
