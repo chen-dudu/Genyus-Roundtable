93e5788be49082ea381330f7a8eb6cff01e1dc61
chen-dudu <liguoc@student.unimelb.edu.au>
2020-09-25 11:03:38 +0800

    added property status for application class

--- src/DataModel/ApplicationModel/Application.js
+++ src/DataModel/ApplicationModel/Application.js
@@ -6,12 +6,14 @@ class Application {
      * @param aid      the unique id of the application
      * @param uid      the id of the applicant
      * @param sid      the id of the roundtable session to be applied for
+     * @param status   the current status of the application, i.e. approved, rejected or pending for assessment
      * @param optional additional information applicant provides during the application process
      */
-    constructor(aid, uid, sid, optional='') {
+    constructor(aid, uid, sid, status, optional='') {
         this._aid = aid;
         this._uid = uid;
         this._sid = sid;
+        this._status = status;
         this._optional = optional;
     }
 
@@ -25,6 +27,10 @@ class Application {
 
     get sid() {
         return this._sid;
+    }
+
+    get status() {
+        return this._status;
     }
 
     get optional() {
@@ -41,6 +47,10 @@ class Application {
 
     set sid(newID) {
         this._sid = newID;
+    }
+
+    set status(newStatus) {
+        this._status = newStatus;
     }
 
     set optional(newOptional) {
