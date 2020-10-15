25368b8f74e3d4fec2c34090283a5313bcc00039
chen-dudu <liguoc@student.unimelb.edu.au>
2020-10-15 20:58:37 +0800

    added attribute notification to pod model

--- src/FoundationLayer/PodModel/Pod.js
+++ src/FoundationLayer/PodModel/Pod.js
@@ -3,18 +3,20 @@ class Pod {
 
     /**
      * a private constructor which will be called by pod manager to creat a new pod object
-     * @param pid          the unique id of the pod
-     * @param title        the title of the pod
-     * @param calendlyLink the calendly sharing link of the pod
-     * @param researcher   the researcher who is responsible for the pod
-     * @param participants the list of participants who sign up for the pod
+     * @param pid           the unique id of the pod
+     * @param title         the title of the pod
+     * @param calendlyLink  the calendly sharing link of the pod
+     * @param researcher    the researcher who is responsible for the pod
+     * @param participants  the list of participants who sign up for the pod
+     * @param notifications the list of notifications associated with the pod
      */
-    constructor(pid, title, calendlyLink, researcher, participants) {
+    constructor(pid, title, calendlyLink, researcher, participants, notifications) {
         this._pid = pid;
         this._title = title;
         this._calendlyLink = calendlyLink;
         this._researcher = researcher;
         this._participants = participants;
+        this._notifications = notifications;
         // this._sessions = sessions
     }
 
@@ -38,9 +40,9 @@ class Pod {
         return this._participants;
     }
 
-    // get sessions() {
-    //     return this._sessions;
-    // }
+    get notifications() {
+        return this._notifications;
+    }
 
     set pid(newID) {
         this._pid = newID;
@@ -58,9 +60,9 @@ class Pod {
         this._researcher = newResearcher;
     }
 
-    // addSession(session) {
-    //     this._sessions.unshift(session);
-    // }
+    addNotification(notification) {
+        this._notifications.unshift(notification);
+    }
 
     addParticipant(participant) {
         this._participants.unshift(participant);
@@ -73,10 +75,10 @@ class Pod {
         }
     }
 
-    deleteSession(session) {
-        let index = this._sessions.indexOf(session);
+    deleteNotification(notification) {
+        let index = this._notifications.indexOf(notification);
         if (index !== -1) {
-            this._sessions.splice(index, 1);
+            this._notifications.splice(index, 1);
         }
     }
 }
