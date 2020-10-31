369916f944f7eec050adf6228c51a4d153023f80
AKAME <kaixuang@student.unimelb.edu.au>
2020-10-31 17:14:20 +0800

    add comments for the reseacherpodlist page

--- src/View/ResearcherUI/ResearcherPodList/Header.js
+++ src/View/ResearcherUI/ResearcherPodList/Header.js
@@ -8,8 +8,15 @@ import { HeaderWrapper, ImageWrapper, Seperator } from './ResearcherPodList.styl
 import { UserOutlined } from "@ant-design/icons";
 import UserManager from '../../../DataModel/UserModel/UserManager';
 
+/**
+ *@Description: a react component that renders the header of the ResearcherPodList
+ */
 
 class Header extends React.Component {
+    /**
+     * render the JSX elements
+     * @returns {JSX.Element} including a logo and the avatar of the current user and a button to logout
+     */
     handleClick = () => {
         UserManager.logout()
             .then(response => {
@@ -18,7 +25,6 @@ class Header extends React.Component {
             .catch(error => {
                 console.log(error);
             });
-
         this.props.history.push('/');
     }
 
@@ -26,15 +32,12 @@ class Header extends React.Component {
         return (
             <HeaderWrapper>
                 <ImageWrapper>
-
                     <img src={logo} />
                     {/* <Avatar src={this.props.image} size={64} style={{ position: "absolute", right: "30%", top: "3%" }} icon={<UserOutlined />} />
                     <Button danger style={{ right: "20%", margin: '1% 3%' }} onClick={() => this.props.history.push('../')} >Logout</Button> */}
                     <Avatar onClick={()=>{this.props.history.push("/ResearcherHomePage")}} src={this.props.image} size={64} style={{margin: '0% auto', position: "absolute", left: "85%", top: "3%" }} icon={<UserOutlined />} />
                     <Button danger style={{ left: "90%", margin: '1% auto' }} onClick={this.handleClick} >logout</Button>
-
                 </ImageWrapper>
-
                 <Seperator />
             </HeaderWrapper>
 
--- src/View/ResearcherUI/ResearcherPodList/ResearcherPodList.js
+++ src/View/ResearcherUI/ResearcherPodList/ResearcherPodList.js
@@ -8,18 +8,23 @@ import Body from './Body.js';
 import { withRouter } from 'react-router-dom'
 import Header from './Header.js'
 
+/**
+ *@Description: a react component that renders the ResearcherPodList
+ */
+
 const ResearcherPodList = props => {
     const [image, setImage] = useState();
-
+    /**
+     * render the JSX elements
+     * @returns {JSX.Element} including the header, the body and the footer
+     */
     return(
       <container>
-          <Header image={image}></Header>
-          <Body setImage={setImage}></Body>
+          <Header image={image}/>
+          <Body setImage={setImage}/>
           <Footer/>
       </container>
     )
   }
 
-
-// ========================================
 export default withRouter(ResearcherPodList);
