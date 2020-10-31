32ccf65ecadd83ffe37a0761f2e48416e33b79d1
Yujun Yan <yujunyan@Yujuns-MacBook-Pro.local>
2020-11-01 03:01:36 +1100

    check participant home page

--- src/View/ParticipantUI/ProfileSetting/ProfileSetting.js
+++ src/View/ParticipantUI/ProfileSetting/ProfileSetting.js
@@ -1,3 +1,9 @@
+/**
+ * The ProfileSetting Component contains:
+ * Header: logo and avatar
+ * Body: upload button, input fields, submitting button
+ * Footer: LeftWrapper, MidWrapper, RightWrapper
+ */
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import logo from '../../../img/logo.png';
@@ -6,7 +12,11 @@ import Footer from './Footer.js';
 import Body from './Body.js';
 import {withRouter} from 'react-router-dom';
 
-
+/** 
+ * setImage is used to unify 
+ * the image upload in the Body component
+ * and the avatar in the Header component
+*/
 const ProfileSetting = props => {
   const [image, setImage] = useState();
   console.log("ProfileSetting***",image);
