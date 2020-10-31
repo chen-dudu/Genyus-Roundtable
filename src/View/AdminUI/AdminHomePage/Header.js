4f5a388e38aed94ea1ae7007d610593d71e02ca1
Yujun Yan <yujunyan@Yujuns-MacBook-Pro.local>
2020-10-31 21:46:30 +1100

    check AdminHomePage

--- src/View/AdminUI/AdminHomePage/Header.js
+++ src/View/AdminUI/AdminHomePage/Header.js
@@ -1,3 +1,8 @@
+/**
+ * The Header is composed by
+ * ImageWapper holding the GN logo
+ * LogoutWrapper holding the Avatar and the logout button
+ */
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { HeaderWrapper, ImageWrapper, Seperator, LogoutWrapper } from './AdminHomePage.style';
@@ -9,6 +14,10 @@ import UserManager from '../../../DataModel/UserModel/UserManager';
 
 
 class Header extends React.Component {
+
+  /**
+   * logout after clicking the logout button
+   */
   handleClick = () => {
     UserManager.logout()
       .then(response => {
